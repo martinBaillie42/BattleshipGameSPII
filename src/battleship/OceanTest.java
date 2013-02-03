961d7cc84957ed83cc64823bee0af926f8b13a81
Martin Baillie <martin@martinbaillie.net>
2013-02-03 17:34:43 +0000

    Generated testClasses ShipTest and OceanTest

---
+++ src/battleship/OceanTest.java
@@ -0,0 +1,52 @@
+package battleship;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Martin Martin
+ * Date: 03/02/13
+ * Time: 17:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class OceanTest {
+    @Test
+    public void testPlaceAllShipsRandomly() throws Exception {
+
+    }
+
+    @Test
+    public void testIsOccupied() throws Exception {
+
+    }
+
+    @Test
+    public void testShootAt() throws Exception {
+
+    }
+
+    @Test
+    public void testGetShotsFired() throws Exception {
+
+    }
+
+    @Test
+    public void testGetHitCount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetShipsSunk() throws Exception {
+
+    }
+
+    @Test
+    public void testIsGameOver() throws Exception {
+
+    }
+
+    @Test
+    public void testGetShipArray() throws Exception {
+
+    }
+}
---
+++ src/battleship/ShipTest.java
@@ -0,0 +1,67 @@
+package battleship;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Martin Martin
+ * Date: 03/02/13
+ * Time: 17:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShipTest {
+    @Test
+    public void testGetBowRow() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBowColumn() throws Exception {
+
+    }
+
+    @Test
+    public void testIsHorizontal() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLength() throws Exception {
+
+    }
+
+    @Test
+    public void testSetBowColumn() throws Exception {
+
+    }
+
+    @Test
+    public void testSetBowRow() throws Exception {
+
+    }
+
+    @Test
+    public void testSetHorizontal() throws Exception {
+
+    }
+
+    @Test
+    public void testOkToPlaceShipAt() throws Exception {
+
+    }
+
+    @Test
+    public void testPlaceShipAt() throws Exception {
+
+    }
+
+    @Test
+    public void testShootAt() throws Exception {
+
+    }
+
+    @Test
+    public void testIsSunk() throws Exception {
+
+    }
+}
